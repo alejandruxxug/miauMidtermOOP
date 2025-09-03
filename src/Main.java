7867a68f253f0abca6c07f2926fb4535055bd342
alejandruxxug <alejandro.urrego@eia.edu.co>
2025-09-03 11:49:48 -0500

    Adding more sample data

--- src/Main.java
+++ src/Main.java
@@ -32,13 +32,14 @@ public class Main {
         Carro c3 = new Carro("HJU890", "Civic", 2019);
         Carro c4 = new Carro("LST650", "Picanto", 2019);
         Carro c5 = new Carro("NHW914", "Koleos", 2019);
+
         carros.add(c1);
         carros.add(c2);
         carros.add(c3);
 
         Dueno dueno = new Dueno(123456789, "Seb", "Zap", "555-1234");
-        Dueno dueno2 = new Dueno(987654321, "Jane", "Smith", "555-5678");
-        Dueno dueno3 = new Dueno(1283991, "Alejo", "Urrego", "555-5678");
+        Dueno dueno2 = new Dueno(987654321, "Majo", "sussy", "555-5678");
+        Dueno dueno3 = new Dueno(1283991, "Alejo", "Urrego", "213-5678");
 
         duenos.add(dueno);
         duenos.add(dueno2);
@@ -58,7 +59,29 @@ public class Main {
         c4.agregarComentario("El carro 1 de alejo jajjasj", "2024-01-20");
         c5.agregarComentario("El carro 2 de alejo que carro mas tragonn", "2024-01-20");
 
+        Marca m1 = new Marca(1, "Renault", "France");
+        Marca m2 = new Marca(2, "Ford", "USA");
+        Marca m3 = new Marca(3, "Honda", "Japan");
+        Marca m4 = new Marca(4, "Kia", "South Korea");
+
+        marcas.add(m1);
+        marcas.add(m2);
+        marcas.add(m3);
+        marcas.add(m4);
+
+        m1.agregarCarro(carros.get(0));
+        m1.agregarCarro(carros.get(4));
+        m2.agregarCarro(carros.get(1));
+        m3.agregarCarro(carros.get(2));
+        m4.agregarCarro(carros.get(3));
+
+
         Incidente i1 = new Incidente(1, "Collision", "2023-10-05", "555-1234");
+        Incidente i2 = new Incidente(2, "Exploto", "2023-10-05", "5we55-1234");
+        Incidente i3 = new Incidente(3, "Devoro el carro", "2023-10-05", "552345-1234");
+        Incidente i4 = new Incidente(4, "Miau", "2023-10-05", "555234-12354");
+
+
 
 
 
@@ -67,6 +90,10 @@ public class Main {
             option = number.nextInt();
             switch (option) {
                 case 1:
+                    //Most Sold Brand
+                    Marca mostSold = null;
+                    int maxCars = 0;
+
                     break;
                 case 2:
                     break;
@@ -75,7 +102,7 @@ public class Main {
                 case 4:
                     done = true;
                     System.out.println("Slay... See you again next time!~");
-                    break:
+                    break;
                 default:
                     System.out.println("Hey bitch is you dumb?? Place a correct number..");
             }
